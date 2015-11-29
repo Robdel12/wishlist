bf61f1b619e7e2d158679858c0f45f6899f16502
Robdel12 <robertdeluca19@gmail.com>
2015-11-28 19:09:10 -0600

    Add scrolltop to route changes

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import ScrollReset from '../mixins/route-scroller'
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ScrollReset, {
 });
--- app/routes/wishlist/show.js
+++ app/routes/wishlist/show.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import ScrollReset from '../../mixins/route-scroller'
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ScrollReset, {
   model(params) {
     return this.store.find('wishlist', params.id);
   }
