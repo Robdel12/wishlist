b362fbf85168c147b36f33088c3c05bd4eadfdbb
Robdel12 <robertdeluca19@gmail.com>
2015-11-28 21:10:17 -0600

    Clear new wish form when navigating away

--- app/routes/wishlist/new.js
+++ app/routes/wishlist/new.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  deactivate() {
+    this._super.apply(this, arguments);
+    let controller = this.controllerFor('wishlist.new');
+
+    controller.setProperties({
+      itemName: null,
+      imgURL: null,
+      price: null,
+      description: null,
+      itemURL: null,
+      nameOfStore: null,
+      submittedBy: null,
+      quantity: null,
+      validURL: false,
+      submittedByDisplayName: null
+    });
+  }
 });
